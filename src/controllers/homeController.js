e8be8693aa30e2a6b620db53d1990aeca5e6b694
thecuong1396 <cuongphan@coinmap.tech>
2023-05-04 13:59:38 +0700

    get list users

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -7,16 +7,19 @@ let getHome = async (req, res) => {
     return res.render("home");
 };
 let getFormUser = async (req, res) => {
-    return res.render("create-user.ejs");
+    let userList = await getListUserService();
+    console.log("userList", userList);
+    return res.render("create-user.ejs", { userList });
 };
 let postSubmitCreateUser = (req, res) => {
     createUserService(req.body);
     return res.redirect("/create-user");
 };
 
-let getUserList = () => {
-    getListUserService();
-    return res.redirect("/create-user");
+let getUserList = async (req, res) => {
+    let userList = await getListUserService();
+    console.log("userList", userList);
+    return res.render("create-user.ejs", { userList });
 };
 
 module.exports = {
--- src/services/userServices.js
+++ src/services/userServices.js
@@ -1,5 +1,7 @@
-import connection from "../configs/connectDB";
+// import connection from "../configs/connectDB";
 import bcrypt from "bcrypt";
+import mysql from "mysql2/promise";
+import bluebird from "bluebird";
 const salt = bcrypt.genSaltSync(10);
 
 const hashUserPassword = (passwordUser) => {
@@ -7,26 +9,55 @@ const hashUserPassword = (passwordUser) => {
 };
 
 const createUserService = async (data) => {
+    const connection = await mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        database: "fullstack",
+        Promise: bluebird,
+    });
     let hashPass = hashUserPassword(data.password);
-    connection.query(
-        "insert into users(email, username, password) values (?, ?, ?)",
-        [data.email, data.username, hashPass],
-        function (err, results, fields) {
-            if (err) {
-                console.log("err", err);
-            }
-        }
-    );
+    try {
+        const [rows, fields] = await connection.execute(
+            "insert into users(email, username, password) values (?, ?, ?)",
+            [data.email, data.username, hashPass]
+        );
+        console.log("rows", rows);
+        return rows;
+    } catch (error) {
+        console.log("error", error);
+    }
+    // connection.query(
+    //     "insert into users(email, username, password) values (?, ?, ?)",
+    //     [data.email, data.username, hashPass],
+    //     function (err, results, fields) {
+    //         if (err) {
+    //             console.log("err", err);
+    //         }
+    //     }
+    // );
 };
 
 const getListUserService = async () => {
-    let users = [];
-    connection.query("Select * from users", function (err, results, fields) {
-        if (err) {
-            console.log("err", err);
-        }
-        console.log("check results", results);
+    const connection = await mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        database: "fullstack",
+        Promise: bluebird,
     });
+    let users = [];
+    // connection.query("Select * from users", function (err, results, fields) {
+    //     if (err) {
+    //         console.log("err", err);
+    //     }
+    //     console.log("check results", results);
+    // });
+    try {
+        const [rows, fields] = await connection.execute("Select * from users");
+        console.log("rows", rows);
+        return rows;
+    } catch (error) {
+        console.log("error", error);
+    }
 };
 
 module.exports = {
